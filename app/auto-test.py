4daac1a45626329b45a7a1e14972a7e2b6b38e14
schumixer <schumixer@list.ru>
2020-11-02 13:35:31 +0300

    try except errors fixed

--- app/auto-test.py
+++ app/auto-test.py
@@ -1,13 +1,22 @@
 from classes import *
 def main():
    #read data
-   modelData, studentData, names, resultData = Data.input()
-
+   try:
+      modelData, studentData, names, resultData = Data.input()
+   except:
+      print("read data error:", sys.exc_info()[0])
+      sys.exit(1) # exiing with a non zero value is better for returning from an error
    #edit data
-   students = Main.mainFunction(modelData, studentData, names, resultData)
-
+   try:
+      students = Main.mainFunction(modelData, studentData, names, resultData)
+   except:
+      print("edit data error:", sys.exc_info()[0])
+      sys.exit(1) # exiing with a non zero value is better for returning from an error
    #write data
-   Data.writeData(students, modelData, resultData)
-
+   try:
+      Data.writeData(students, modelData, resultData)
+   except:
+      print("write data error:", sys.exc_info()[0])
+      sys.exit(1) # exiing with a non zero value is better for returning from an error
 if __name__ == '__main__':
    main()
--- app/classes.py
+++ app/classes.py
@@ -135,52 +135,54 @@ class Main():
       #checkRightName = True
       #check out all the students
       for file in studentData["files"]:
-         #opening the file with students works
-         encodingResult=Main.chooseEncoding(f"{studentData['dir']}/{file}")
-         
-         with open(f"{studentData['dir']}/{file}",encoding=encodingResult) as studentFile:
-            studentLines = Main.deleteEmptyLines(studentFile.readlines())
-            configure = studentLines[0]
-            fullName = studentLines[1] = Main.replaceSym(studentLines[1].upper())
-            #checkRightName = Main.checkRightName(configure)
-            #if name of the student was in excel file
-            if fullName in names:
-               #isExist = False
-               studentsNumber = len(students)
-               for i in range(len(students)):
-                  if fullName == students[i].fullName:
-                     studentsNumber = i
-                     #isExist = True
-                     break
-               # if not isExist:
-               #    students.append(Student(fullName))
-               
-               if configure in Data.TruncateExtensions(copy.copy((modelData["files"]))):
-                  #opening the file with model works
-                  with open(f"{modelData['dir']}/{configure+'.txt'}",encoding="cp437") as modelFile:
-                     modelLines = modelFile.readlines()
+         try:
+            #opening the file with students works
+            encodingResult=Main.chooseEncoding(f"{studentData['dir']}/{file}")
+            
+            with open(f"{studentData['dir']}/{file}",encoding=encodingResult) as studentFile:
+               studentLines = Main.deleteEmptyLines(studentFile.readlines())
+               configure = studentLines[0]
+               fullName = studentLines[1] = Main.replaceSym(studentLines[1].upper())
+               #checkRightName = Main.checkRightName(configure)
+               #if name of the student was in excel file
+               if fullName in names:
+                  #isExist = False
+                  studentsNumber = len(students)
+                  for i in range(len(students)):
+                     if fullName == students[i].fullName:
+                        studentsNumber = i
+                        #isExist = True
+                        break
+                  # if not isExist:
+                  #    students.append(Student(fullName))
+                  
+                  if configure in Data.TruncateExtensions(copy.copy((modelData["files"]))):
+                     #opening the file with model works
+                     with open(f"{modelData['dir']}/{configure+'.txt'}",encoding="cp437") as modelFile:
+                        modelLines = modelFile.readlines()
 
-                     isRight = True
+                        isRight = True
 
 
-                     if(len(studentLines)==len(modelLines)+2):
+                        if(len(studentLines)==len(modelLines)+2):
 
-                        for i in range(len(modelLines)):
+                           for i in range(len(modelLines)):
 
-                           if studentLines[i+2].strip()!=modelLines[i].strip():
-                              students[studentsNumber].currentResults[configure]="-"
-                              isRight = False
-                              break
+                              if studentLines[i+2].strip()!=modelLines[i].strip():
+                                 students[studentsNumber].currentResults[configure]="-"
+                                 isRight = False
+                                 break
 
-                        if isRight:
-                           students[studentsNumber].currentResults[configure]="+"
+                           if isRight:
+                              students[studentsNumber].currentResults[configure]="+"
 
-                     else: 
-                        students[studentsNumber].currentResults[configure]="-"
+                        else: 
+                           students[studentsNumber].currentResults[configure]="-"
 
-               else:
-                  students[studentsNumber].currentResults[configure]="-"
-
+                  else:
+                     students[studentsNumber].currentResults[configure]="-"
+         except:
+            print("Unexpected error:", sys.exc_info()[0])
       #addition undone configures
       for i in range(len(students)):
          for item in Data.TruncateExtensions(copy.copy((modelData["files"]))):
